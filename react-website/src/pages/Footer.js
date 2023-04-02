f3ddc8062b35c9bd6c432dff1e2e89a83c557335
andrewcargill <andycargill01@aol.com>
2023-04-02 15:09:10 +0000

    header will load content of footer

--- react-website/src/pages/Footer.js
+++ react-website/src/pages/Footer.js
@@ -7,12 +7,35 @@ const Footer = () => {
 
     const{mainContent} = useContext(MainContentContext)
 
-
+    const renderContent = () => {
+        console.log({mainContent})
+        switch (mainContent) {
+            case "Home":
+                return(
+                    <div>
+                        <h1>HOME PAGE</h1>
+                    </div>
+                );
+            case "About":
+                return (
+                    <div>
+                        <h1>About Page</h1>
+                    </div>
+                );
+            case "Project":
+                return (
+                    <h1>Project</h1>
+                );
+            default:
+                return ('none');
+        }
+        
+    }
 
     return (
         <div id="header">This is the footer
+        <div>{renderContent()}</div>
         <p>{mainContent}</p>
-        <p></p>
         </div>
         
 
