14fc3c5e852b0958c91ada78318db6659b23e8ae
andrewcargill <andycargill01@aol.com>
2023-04-03 05:15:50 +0000

    half way through setting up to pass a prop to the MainText component

--- react-website/src/pages/Header.js
+++ react-website/src/pages/Header.js
@@ -8,21 +8,22 @@ import MainText from "./MainText";
 const Header = () => {
 
   const [mainText, setMainText] = useState('Default content');
-  const { updateMainContent } = useContext(MainContentContext);
+  // const { updateMainContent } = useContext(MainContentContext);
 
-  const OnClickMainContent = (props) => {
-    updateMainContent(props);
+  const onClickMainContent = (props) => {
+    // updateMainContent(props);
     setMainText(props)
-  }
+    console.log(mainText)
+  };
+
   return (
     <div id="header">
-      <Button onClick={() => OnClickMainContent('Home')} text="Home" />
-      <Button onClick={() => OnClickMainContent('About')} text="About" />
-      <Button onClick={() => OnClickMainContent('Project')} text="Project" />
-      <Button onClick={() => OnClickMainContent('Button Button')} text="Button Four" />
-      <Button onClick={() => OnClickMainContent('Button Wow!')} text="Button Five" />
-      <Button onClick={() => OnClickMainContent('Button Help')} text="Button Six" />
-      <Button onClick={() => OnClickMainContent('Button Jesus!')} text="Button Seven" />
+      
+      <Button onClick={() => onClickMainContent('Home')} text="Project" />
+      <Button onClick={() => onClickMainContent('Button Button')} text="Button Four" />
+      <Button onClick={() => onClickMainContent('Button Wow!')} text="Button Five" />
+      <Button onClick={() => onClickMainContent('Button Help')} text="Button Six" />
+      <Button onClick={() => onClickMainContent('Button Jesus!')} text="Button Seven" />
       <div>{mainText}</div>
 
       <div>
